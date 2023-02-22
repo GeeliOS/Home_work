df833018eade3574dae6d78f8d250a35e0558da1
Gleb <gleb_krav@mail.ru>
2023-02-23 01:16:51 +0300

    2 домашнее задание

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.home),
-
+    path('about', views.about, name='about'),
+    path('languages-list', views.languages_list, name='languages-list'),
+    path('countries-list', views.countries_list, name='countries-list'),
+    path('countrie/<str:name>', views.countrie, name='countrie'),
 ]
 
 
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,9 +1,35 @@
+import json
 from django.shortcuts import render, HttpResponse
 
+with open('/home/student/Projets/DjangoCountries/MainApp/sw_templates.json') as f:
+    file_content = f.read()
+    country_info = json.loads(file_content)
 
 
+def about(request):
+    result = f"""
+  <center> <a  href='/countries-list'>Страны</a><br>
+   <a  href='/languages-list'>Языки</a></center>
+    """
+    return HttpResponse(result)
 
-def home(request):
+def countries_list(request):
+    context={
+        "country_info":country_info
+    }
+    return render(request, 'countries-list.html', context)
 
-    return HttpResponse(f"""<center><h1>Приветствую вас !!</h1></center>""")
 
+def languages_list(request):
+    context={
+        "country_info":country_info
+    }
+    return render(request, 'languages-list.html', context)
+
+def countrie(request, name):
+    for countryin in country_info:
+        if countryin['country']==name:
+            context={
+                "countryin":countryin
+            }
+    return render(request, 'countrie.html', context)
