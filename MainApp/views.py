b3db3be1548eb83d617a085de8b99eb87d2b32db
boo <eyurchenko@specialist.ru>
2023-02-25 01:36:13 +0700

    Недоработки по проекту

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,10 +1,13 @@
 import json
 from django.shortcuts import render, HttpResponse
 
+# TODO: использование абсолютных путей недопустимо, используйте пути относительно корня проекта
 with open('/home/student/Projets/DjangoCountries/MainApp/sw_templates.json') as f:
     file_content = f.read()
     country_info = json.loads(file_content)
 
+# TODO: директория виртуального окружения(venv) не должна быть часть git проекта.
+#  Исправите файл .gitignore и удалите виртуальное окружение из под репозитория
 
 def about(request):
     result = f"""
@@ -21,6 +24,7 @@ def countries_list(request):
 
 
 def languages_list(request):
+    # TODO: тут вам нужно создать список языков, исключив дубликаты и передать в шаблон. А в шаблоне отобразить языки
     context={
         "country_info":country_info
     }
